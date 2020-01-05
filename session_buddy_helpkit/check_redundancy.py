30bbb2950d7b48c0041e6639482a55ba6758ccb0
MapleCCC <littlelittlemaple@gmail.com>
2020-01-05 21:19:18 +0800

    Add check_redundancy utility, written in imperative style.

--- session_buddy_helpkit/check_redundancy.py
+++ session_buddy_helpkit/check_redundancy.py
@@ -8,6 +8,49 @@ from typing import *
 from .utils.hash_utils import hashablize_dict
 
 
+Digest = namedtuple("Digest", ["filename", "fingerprint"])
+
+
+def load_json_from_file(filepath: str) -> Dict:
+    with open(filepath, "r", encoding="utf-8-sig") as f:
+        return json.load(f)
+
+
+def extract_digests(filepaths: List[str]) -> List[Digest]:
+    digests = []
+    for filepath in filepaths:
+        json_obj = load_json_from_file(filepath)
+        sessions = json_obj["sessions"]
+        fingerprint = frozenset(hash(hashablize_dict(session)) for session in sessions)
+        digests.append(
+            Digest(filename=os.path.basename(filepath), fingerprint=fingerprint)
+        )
+    return digests
+
+
+def calculate_sinks(digests: List[Digest]) -> List[Digest]:
+    sinks = []
+    for digest in digests:
+        new = []
+        for sink in sinks:
+            if sink[1].issubset(digest[1]):
+                sinks.remove(sink)
+        new.append(digest)
+        sinks = new
+    return sinks
+
+
+def check_redundancy_imperative_style(filepaths: List[str]) -> None:
+    digests = extract_digests(filepaths)
+    digests.sort(key=lambda digest: len(digest.fingerprint))
+    sinks = calculate_sinks(digests)
+
+    print(f"Scanned {len(filepaths)} files")
+    print(f"{len(sinks)} of them are sinks")
+    # for sink in sinks:
+    #     print(sink.filename)
+
+
 # TODO: what's the best practice on exception handling in functional programming
 def check_redundancy_functional_style(filepaths: List[str]) -> None:
     Digest = namedtuple("Digest", ["filename", "fingerprint"])
