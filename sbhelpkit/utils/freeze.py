9f71e96c30842342d5e60f16f1d3d35c5ca122a1
MapleCCC <littlelittlemaple@gmail.com>
2020-01-10 02:32:04 +0800

    Various minor updates to freeze.py

--- sbhelpkit/utils/freeze.py
+++ sbhelpkit/utils/freeze.py
@@ -1,22 +1,38 @@
 """
 If the purpose of freezing list or dict is just want to get a hash digest,
 then use `hash_list` and `hash_dict` directly instead of use `hash(freeze_list(l))`
-or `hash(freeze_dict(d))`. They are faster and more space efficient by using the technique of
-lazy evaluation without the need to construct a whole large object in the memory, which could
-be both time costly and space costly.
+or `hash(freeze_dict(d))`. They are faster and more space efficient by using the technique
+of lazy evaluation over a stream of input data, without the need to construct a whole large
+object in the memory, which could be both time costly and space costly.
+
+Note that hash value produced by `hash_list` and `hash_dict` are not stable. Don't expect to
+get identical value in different versions of Python interpreter, or even different run of the
+same interpreter.
+
+If you want stable and persistent hash result, try `hashlib`.
 """
 
-from typing import *
-from .pyobjhash import *
 import itertools
+from typing import *
+
+from .pyobjhash import *
+
 
 __all__ = ["freeze", "ihash"]
 
+# sentinel pattern
+# these objects are immutable global singletons within a session of Python interpreter
+# which implies that you should not account on them to produce some stable
+# and reproducible data that can persists across different instances of Python interpreters.
+# If you want to have persisitent hash result, try hashlib.
 list_sentinel = object()
 dict_sentinel = object()
 
+# hashable implies immutable. Immutable is a form of freeze
+Immutable = Hashable
 
-def freeze(item) -> Hashable:
+
+def freeze(item) -> Immutable:
     if isinstance(item, Hashable):
         # WARNING: A great gotchar here is that passing isinstance(x, Hashable) test
         # doesn't necessarily imply that hash(x) won't fail.
@@ -61,19 +77,25 @@ def ihash(item) -> int:
         raise TypeError(f"Unhashable type {type(item)}")
 
 
-# TODO: Do we want to have no two different lists mapping to a same tuple,
-# or do we want to have no two different objects mapping to a same tuple.
-# if the latter if true, we need to add some sentinel to mixup the hash
+# add sentinel to mixup the result
 def freeze_list(l: List) -> FrozenSet:
     """
     This function provides a one-to-one mapping from a list instance
-    to a tuple instance. The mapped tuple is guaranteed to contain only hashable
-    elements, hence the tuple itself becomes hashable.
+    to a frozenset instance. The mapped frozenset is guaranteed to contain only hashable
+    elements, hence the frozenset itself becomes hashable and immutable.
 
-    Highly likely no two different lists are mapped to a same tuple.
-    Two eqaul lists yield the same tuple.
+    Highly likely no two different lists are mapped to a same frozenset.
+    Two eqaul lists always yield the same frozenset. That is, the following property
+    always holds: x == y -> hash(x) == hash(y)
 
     If the mapping cannot be found for some lists, a ValueError will be raised.
+
+    Despite the name, argument other than list is also acceptable as input.
+    Any finite iterable also works.
+
+    Known hash collisions are:
+        False and "" and 0
+        -1 and -2 and -1.0 and -2.0
     """
 
     def helper(l: List) -> Generator:
@@ -84,16 +106,21 @@ def freeze_list(l: List) -> FrozenSet:
     return frozenset(helper(l))
 
 
+# add sentinel to mix up the result
 def freeze_dict(d: Dict) -> FrozenSet:
     """
     This function provides a one-to-one mapping from a dict instance
-    to a tuple instance. The mapped tuple is guaranteed to contain only hashable
-    elements, hence the tuple itself becomes hashable.
+    to a frozenset instance. The mapped frozenset is guaranteed to contain only hashable
+    elements, hence the frozenset itself becomes hashable and immutable.
 
-    Highly likely no two different dicts are mapped to a same tuple.
-    Two eqaul dicts yield the same tuple.
+    Highly likely no two different dicts are mapped to a same frozenset.
+    Two eqaul dicts always yield the same frozenset. That is, the following property
+    always holds: x == y -> hash(x) == hash(y)
 
     If the mapping cannot be found for some dicts, a ValueError will be raised.
+
+    Despite the name, argument other than list is also acceptable as input.
+    Any finite iterable also works.
 
     Known hash collisions are:
         False and "" and 0
