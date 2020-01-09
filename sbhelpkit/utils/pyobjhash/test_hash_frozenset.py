f297ec7ad15c356ca3b878c5931f13ae7d5fa1cf
MapleCCC <littlelittlemaple@gmail.com>
2020-01-10 00:52:06 +0800

    Add low collision rate testing for hash_tuple and hash_freeze

--- sbhelpkit/utils/pyobjhash/test_hash_frozenset.py
+++ sbhelpkit/utils/pyobjhash/test_hash_frozenset.py
@@ -1,15 +1,17 @@
 from typing import *
 
-from hypothesis import given
+from hypothesis import given, settings, assume
 from hypothesis.strategies import *
 
 from .hash_frozenset import *
 
 hashable_types = none() | booleans() | floats() | text()
 
+# TODO: test on nested frozenset
+
 
 @given(frozensets(hashable_types))
-def test_hash_frozenset(s: FrozenSet) -> None:
+def test_hash_frozenset_regression(s: FrozenSet) -> None:
     assert hash_frozenset(s) == hash(s)
 
 
@@ -21,4 +23,32 @@ def test_frozensethasher(s: FrozenSet) -> None:
     assert sh.digest() == hash(s)
 
 
-# TODO: test on nested frozenset
+# It's hard to instruct hypothesis to generate a sufficient amount of pair of unequal frozensets.
+# So we are gonna use a trick here.
+# Use helper function to exploit hypothesis' search strategies.
+# Use global variable to track intermediate information.
+
+collision_count = 0
+total_count = 0
+acceptable_collision_rate_threshold = 0.03
+total_test_case = 500
+min_test_case = 300
+
+
+@settings(max_examples=total_test_case)
+@given(frozensets(hashable_types), frozensets(hashable_types))
+def test_hash_different_frozenset_low_collision_rate_helper(
+    s1: FrozenSet[Hashable], s2: FrozenSet[Hashable]
+) -> None:
+    global collision_count, total_count
+    assume(s1 != s2)
+    if hash_frozenset(s1) == hash_frozenset(s2):
+        collision_count += 1
+    total_count += 1
+
+
+@given(just(None))
+def test_hash_different_list_low_collision_rate(_: None) -> None:
+    global collision_count, total_count
+    assert total_count >= min_test_case
+    assert (collision_count / total_count) < acceptable_collision_rate_threshold
--- sbhelpkit/utils/pyobjhash/test_hash_tuple.py
+++ sbhelpkit/utils/pyobjhash/test_hash_tuple.py
@@ -1,6 +1,6 @@
 from typing import *
 
-from hypothesis import given
+from hypothesis import given, settings, assume
 from hypothesis.strategies import *
 
 from .hash_tuple import *
@@ -27,3 +27,34 @@ def test_tuplehasher(l: List) -> None:
     for element in t:
         th.update(element)
     assert th.digest() == hash(t)
+
+
+# It's hard to instruct hypothesis to generate a sufficient amount of pair of unequal lists.
+# So we are gonna use a trick here.
+# Use helper function to exploit hypothesis' search strategies.
+# Use global variable to track intermediate information.
+
+collision_count = 0
+total_count = 0
+acceptable_collision_rate_threshold = 0.03
+total_test_case = 500
+min_test_case = 300
+
+@settings(max_examples=total_test_case)
+@given(lists(hashable_types), lists(hashable_types))
+def test_hash_different_list_low_collision_rate_helper(
+    l1: List[Hashable], l2: List[Hashable]
+) -> None:
+    global collision_count, total_count
+    assume(l1 != l2)
+    t1, t2 = tuple(l1), tuple(l2)
+    if hash_tuple(t1) == hash_tuple(t2):
+        collision_count += 1
+    total_count += 1
+
+
+@given(just(None))
+def test_hash_different_list_low_collision_rate(_: None) -> None:
+    global collision_count, total_count
+    assert total_count >= min_test_case
+    assert (collision_count / total_count) < acceptable_collision_rate_threshold
