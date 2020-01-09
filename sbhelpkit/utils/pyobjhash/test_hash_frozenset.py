50147bcb3d853650ce02a723e7171a332c61504b
MapleCCC <littlelittlemaple@gmail.com>
2020-01-10 02:16:40 +0800

    Fix test for low collision rate of hash_frozenset

--- sbhelpkit/utils/pyobjhash/test_hash_frozenset.py
+++ sbhelpkit/utils/pyobjhash/test_hash_frozenset.py
@@ -29,10 +29,10 @@ def test_frozensethasher(s: FrozenSet) -> None:
 # Use global variable to track intermediate information.
 
 collision_count = 0
-total_count = 0
 acceptable_collision_rate_threshold = 0.03
 total_test_case = 500
-min_test_case = 300
+min_distinct_test_case = 300
+seen_test_cases = set()
 
 
 @settings(max_examples=total_test_case)
@@ -40,14 +40,18 @@ min_test_case = 300
 def test_hash_different_frozenset_low_collision_rate_helper(
     s1: FrozenSet[Hashable], s2: FrozenSet[Hashable]
 ) -> None:
-    global collision_count, total_count
     assume(s1 != s2)
+    assume({s1, s2} not in seen_test_cases)
+    seen_test_cases.add(frozenset({s1, s2}))
+
+    global collision_count
     if hash_frozenset(s1) == hash_frozenset(s2):
         collision_count += 1
-    total_count += 1
 
 
 @given(just(None))
 def test_hash_different_list_low_collision_rate(_: None) -> None:
-    assert total_count >= min_test_case
-    assert (collision_count / total_count) < acceptable_collision_rate_threshold
+    assert len(seen_test_cases) > min_distinct_test_case
+    assert (
+        collision_count / len(seen_test_cases)
+    ) < acceptable_collision_rate_threshold
