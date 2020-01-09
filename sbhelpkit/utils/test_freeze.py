aa99cbb506f089c24ef43e929e40db8bb1372113
MapleCCC <littlelittlemaple@gmail.com>
2020-01-09 10:45:16 +0800

    Update type hints in test_freeze

--- sbhelpkit/utils/test_freeze.py
+++ sbhelpkit/utils/test_freeze.py
@@ -5,42 +5,33 @@ from hypothesis.strategies import *
 
 from .freeze import *
 
+hashable_types = none() | booleans() | floats() | text()
 
-@given(lists(integers()))
-def test_freeze_list(l: List[int]) -> None:
+
+@given(lists(hashable_types))
+def test_freeze_list(l: List[Hashable]) -> None:
     assert freeze_list(l) == freeze_list(l)
     assert hash(freeze_list(l)) == hash(freeze_list(l))
 
 
-# TODO: test over list of heterogeneous type of elements
-# @given(lists())
-# def test_freeze_list_soundness(l: List) -> None:
-#     assert freeze_list(l) == freeze_list(l)
-#     assert hash(freeze_list(l)) == hash(freeze_list(l))
-
-
-@given(lists(integers()), lists(integers()))
-def test_freeze_list_inequality(l1: List[int], l2: List[int]) -> None:
+@given(lists(hashable_types), lists(hashable_types))
+def test_freeze_list_inequality(l1: List[Hashable], l2: List[Hashable]) -> None:
     assume(l1 != l2)
     assert freeze_list(l1) != freeze_list(l2)
     assert hash(freeze_list(l1)) != hash(freeze_list(l2))
 
 
-@given(dictionaries(integers(), integers()))
-def test_freeze_dict(d: Dict[int, int]) -> None:
+@given(dictionaries(hashable_types, hashable_types))
+def test_freeze_dict(d: Dict[Hashable, Hashable]) -> None:
     assert freeze_dict(d) == freeze_dict(d)
     assert hash(freeze_dict(d)) == hash(freeze_dict(d))
 
 
-# TODO: test over dictionary of heterogeneous type of items
-# @given(dictionaries())
-# def test_freeze_dict_soundness(d: Dict) -> None:
-#     assert freeze_dict(d) == freeze_dict(d)
-#     assert hash(freeze_dict(d)) == hash(freeze_dict(d))
-
-
-@given(dictionaries(integers(), integers()), dictionaries(integers(), integers()))
-def test_freeze_dict_inequality(ds1: Dict[int, int], ds2: Dict[int, int]) -> None:
+@given(
+    dictionaries(hashable_types, hashable_types),
+    dictionaries(hashable_types, hashable_types),
+)
+def test_freeze_dict_inequality(ds1: Dict[Hashable], ds2: Dict[Hashable]) -> None:
     assume(ds1 != ds2)
     assert freeze_dict(ds1) != freeze_dict(ds2)
     assert hash(freeze_dict(ds1)) != hash(freeze_dict(ds2))
