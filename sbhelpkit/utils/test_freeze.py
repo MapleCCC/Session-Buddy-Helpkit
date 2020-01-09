ae90b28031d2d9608df8a310a0107751e71b1752
MapleCCC <littlelittlemaple@gmail.com>
2020-01-09 10:48:05 +0800

    Enhance unit test test_freeze to cover broader cases.

--- sbhelpkit/utils/test_freeze.py
+++ sbhelpkit/utils/test_freeze.py
@@ -7,6 +7,9 @@ from .freeze import *
 
 hashable_types = none() | booleans() | floats() | text()
 
+# TODO: decide a collision rate as threshold for being acceptable.
+acceptable_collision_rate_threshold = 0.03
+
 
 @given(lists(hashable_types))
 def test_freeze_list(l: List[Hashable]) -> None:
@@ -14,11 +17,27 @@ def test_freeze_list(l: List[Hashable]) -> None:
     assert hash(freeze_list(l)) == hash(freeze_list(l))
 
 
+freeze_list_test_collision_count = 0
+freeze_list_test_total_count = 0
+
+
 @given(lists(hashable_types), lists(hashable_types))
 def test_freeze_list_inequality(l1: List[Hashable], l2: List[Hashable]) -> None:
+    global freeze_list_test_collision_count, freeze_list_test_total_count
     assume(l1 != l2)
     assert freeze_list(l1) != freeze_list(l2)
-    assert hash(freeze_list(l1)) != hash(freeze_list(l2))
+    if hash(freeze_list(l1)) == hash(freeze_list(l2)):
+        freeze_list_test_collision_count += 1
+    freeze_list_test_total_count += 1
+
+
+@given(just(None))
+def test_freeze_list_low_collision_rate(_: None) -> None:
+    global freeze_list_test_collision_count, freeze_list_test_total_count
+    assert freeze_list_test_total_count >= 100
+    assert (
+        freeze_list_test_collision_count / freeze_list_test_total_count
+    ) < acceptable_collision_rate_threshold
 
 
 @given(dictionaries(hashable_types, hashable_types))
@@ -27,11 +46,29 @@ def test_freeze_dict(d: Dict[Hashable, Hashable]) -> None:
     assert hash(freeze_dict(d)) == hash(freeze_dict(d))
 
 
+freeze_dict_test_total_count = 0
+freeze_dict_test_collision_count = 0
+
+
 @given(
     dictionaries(hashable_types, hashable_types),
     dictionaries(hashable_types, hashable_types),
 )
-def test_freeze_dict_inequality(ds1: Dict[Hashable], ds2: Dict[Hashable]) -> None:
-    assume(ds1 != ds2)
-    assert freeze_dict(ds1) != freeze_dict(ds2)
-    assert hash(freeze_dict(ds1)) != hash(freeze_dict(ds2))
+def test_freeze_dict_inequality(
+    d1: Dict[Hashable, Hashable], d2: Dict[Hashable, Hashable]
+) -> None:
+    global freeze_dict_test_collision_count, freeze_dict_test_total_count
+    assume(d1 != d2)
+    assert freeze_dict(d1) != freeze_dict(d2)
+    if hash(freeze_dict(d1)) == hash(freeze_dict(d2)):
+        freeze_dict_test_collision_count += 1
+    freeze_dict_test_total_count += 1
+
+
+@given(just(None))
+def test_freeze_dict_low_collision_rate(_: None) -> None:
+    global freeze_dict_test_total_count, freeze_dict_test_collision_count
+    assert freeze_dict_test_total_count >= 100
+    assert (
+        freeze_dict_test_collision_count / freeze_dict_test_total_count
+    ) < acceptable_collision_rate_threshold
